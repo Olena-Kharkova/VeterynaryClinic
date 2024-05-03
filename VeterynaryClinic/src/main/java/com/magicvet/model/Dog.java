707e023bd1d83483f2b311fc4d530e251608b97a
Olena Kharkova <helen.kha.ua@gmail.com>
2024-05-03 17:34:26 +0300

    Req-04:Show client info (with pet)

--- VeterynaryClinic/src/main/java/com/magicvet/model/Dog.java
+++ VeterynaryClinic/src/main/java/com/magicvet/model/Dog.java
@@ -1,8 +1,37 @@
 package main.java.com.magicvet.model;
 
+import java.util.Objects;
+
 public class Dog extends Pet{
 
     private String size;
+
+    @Override
+    public String toString() {
+        return "Dog{" +
+                "type='" + getType() + '\'' +
+                ", sex='" + getSex() + '\'' +
+                ", age='" + getAge() + '\'' +
+                ", name='" + getName() + '\'' +
+                ", ownerName='" + getOwnerName() + '\'' +
+                ", size='" + size + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Dog dog = (Dog) o;
+        return Objects.equals(size, dog.size);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), size);
+    }
+
 
     public String getSize() {
         return size;
