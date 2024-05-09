830162302ba29452e852adcff8d33ea8d3b3eac2
Olena Kharkova <helen.kha.ua@gmail.com>
2024-05-09 17:52:30 +0300

    Req-07:Code improvements

--- VeterynaryClinic/src/main/java/com/magicvet/service/PetService.java
+++ VeterynaryClinic/src/main/java/com/magicvet/service/PetService.java
@@ -38,7 +38,8 @@ public class PetService {
 
         if(type.equals(DOG_TYPE)){
             System.out.println("Size [XS / S / M / L/ XXL]:");
-            ((Dog) pet).setSize(Main.SCANNER.nextLine());
+            String size = Main.SCANNER.nextLine();
+            ((Dog) pet).setSize(Dog.Size.valueOf(size));
         }
 
         return  pet;
